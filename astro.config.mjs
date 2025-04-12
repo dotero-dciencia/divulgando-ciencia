9121132139595890a4083561607a62b1800f4744
Neplod <dorian.ruvel@gmail.com>
2025-04-12 18:49:42 +0200

    Latex

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
 
   markdown: {
     syntaxHighlight: false,
